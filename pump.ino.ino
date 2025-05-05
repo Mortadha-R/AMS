3bf500aaee238cda154bef8380a96b425e88531b
Mnddh <124045749+Mortadha-R@users.noreply.github.com>
2025-05-05 11:10:07 +0300

    Add files via upload

---
+++ pump.ino.ino
@@ -0,0 +1,15 @@
+int state =0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  state = digitalRead(2);
+  digitalWrite(3, !state);
+
+
+}
